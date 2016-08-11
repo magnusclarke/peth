639c159d97841f0f66359b22ff5bcb2d8e574496
Magnus Clarke <mclarke3@shef.ac.uk>
2016-08-11 19:57:20 +0100

    ellipses

--- R/sim.R
+++ R/sim.R
@@ -1,4 +1,4 @@
-# functions to simulate density-dependent phylogenetic data
+# Functions to simulate density-dependent phylogenetic data
 # magnusclarke@gmail.com
 # modified 2016
 
@@ -121,39 +121,11 @@ symp_matrix = function(tree, delay=0)
 #--------------------------------------------------------------------------------------#
 #---------- Generate vcv-matrix of simulated trees. -----------------------------------#
 #--------------------------------------------------------------------------------------#
-as_vcv	= function(tree, sigma=1, a=0, reps=1e4, dt=0.01) 
+as_vcv	= function(reps=1e4, ...)
 {
-	x	= t( replicate(reps, sim(tree, sigma=sigma, dt=dt, a=a)$tval))
+	args = as.list(substitute(list(...)))[-1L]
+	x	= t( replicate(reps, do.call(sim, args)$tval, simplify='matrix') )
 	return( cov(x) )
-}
-
-#--------------------------------------------------------------------------------------#
-#---- Get difference between given data and a single simulated dataset. ---------------#
-#--------------------------------------------------------------------------------------#
-get_dif	= function(tree, data, a, sigma, dt=1, nTraits=1, use_K=FALSE, symp) 
-{
-	ntips	= length(data[,1])
-	nTraits	= length(data[1,])
-
-	new		= sim(tree=tree, a=a, sigma=sigma, dt=dt, ntraits=nTraits, symp=symp)$tval
-	
-    difs					= as.matrix(dist(data))			# Euclidian distance
-	difs[which(difs==0)]	= NA							# Ignore matrix diagonal
-	Dgap					= apply(difs, 1, min, na.rm=T)	
-
-	difs					= as.matrix(dist(new))			
-	difs[which(difs==0)]	= NA						
-	Ngap					= apply(difs, 1, min, na.rm=T)
-
-	# Summary statistics: mean and sd of gaps between neighbours. Plus Blomberg's K optionally.
-    if(use_K)
-    {
-		dataK 	= tryCatch(Kcalc(data[,1], tree, F), error=function(err){return(1)})
-		newK 	= tryCatch(Kcalc(new[,1], tree, F), error=function(err){return(1)})
-		return( (mean(Dgap) - mean(Ngap))^2 + (sd(Dgap) - sd(Ngap))^2 + (dataK - newK)^2 )
-    } else {
-		return( (mean(Dgap) - mean(Ngap))^2 + (sd(Dgap) - sd(Ngap))^2 )
-	}
 }
 
 #--------------------------------------------------------------------------------------#
@@ -188,16 +160,15 @@ summary_stats = function(tree, data, use_K=FALSE)
 #--------------------------------------------------------------------------------------#
 #-------- Generate file with many simulation parameters and summary statistics --------#
 #--------------------------------------------------------------------------------------#
-param_stats = function(tree, file='param_stats.out', reps=1e3, max_sigma=8, max_a=8, 
-				symp=NA, allo = NA, dt=0.001, use_K=FALSE, ntraits=1)
+param_stats = function(tree, file='param_stats.out', reps=1e3, max_sigma=8, max_a=8, ...)
 {
 	sig = runif(reps, 0, max_sigma)
 	atry = runif(reps, 0, max_a)
 
 	for(i in 1:reps)
 	{
-		d = sim(tree=tree, a=atry[i], sigma=sig[i], dt=dt, ntraits=ntraits, symp=symp, allo=allo)$tval
-		stats = summary_stats(tree=tree, data=d, use_K=use_K)
+		d = sim(tree=tree, a=atry[i], sigma=sig[i], ...)$tval
+		stats = summary_stats(data=d, ...) 
 	   	write(c(sig[i], atry[i], stats), file=file, append=TRUE, sep=",")
 	}
 }
@@ -205,7 +176,7 @@ param_stats = function(tree, file='param_stats.out', reps=1e3, max_sigma=8, max_
 #--------------------------------------------------------------------------------------#
 #---------- Likelihood ratio: BM versus competition -----------------------------------#
 #--------------------------------------------------------------------------------------#
-lrt	= function(tree, data, file=NA, posteriorSize=500, use_K=FALSE, dt=0.001, max_sigma=8, max_a=8, sim_dat=NA)
+lrt	= function(file=NA, posteriorSize=500, use_K=FALSE, sim_dat=NA, max_sigma=8, max_a=8, ...)
 {
    	# Read simulations into R
 	if(is.na(sim_dat))
@@ -220,7 +191,7 @@ lrt	= function(tree, data, file=NA, posteriorSize=500, use_K=FALSE, dt=0.001, ma
    	stat2 = x[,4]
 
 	# Get summary stats for the true data
-	tstat = summary_stats(tree=tree, data=data, use_K=use_K)
+	tstat = summary_stats(use_K=use_K, ...)
 	tstat1 = tstat[1]
 	tstat2 = tstat[2]
 
@@ -259,243 +230,5 @@ lrt	= function(tree, data, file=NA, posteriorSize=500, use_K=FALSE, dt=0.001, ma
 	return( data.frame(H0_est, H0_lik, H1_est, H1_lik, LRT) )
 }
 
-
-lrt_unnested   = function(tree, data, file=NA, posteriorSize=500, use_K=FALSE, reps=1e4,
-						dt=0.001, max_sigma=8, max_a=8, min_param=0, max_param=8, prior=NA, nullh='EB', post2=100)
-{
-   	# Read simulations into R
-	if(is.na(prior))
-	{
-		x 	= read.csv(file)
-	} else {
-		x = prior
-	}
-   	sig = x[,1]
-   	param = x[,2]
-   	stat1 = x[,3]
-   	stat2 = x[,4]
-   	model = x[,5]
-
-	# Get summary stats for the true data
-	tstat = summary_stats(tree=tree, data=data, use_K=use_K)
-	tstat1 = tstat[1]
-	tstat2 = tstat[2]
-
-	if(use_K)
-	{
-		stat3 = x[,6]
-		tstat3 = tstat[3]
-		diff = abs(stat1-tstat1)  * abs(stat2-tstat2) * abs(stat3 - tstat3)
-	} else {
-		diff = abs(stat1-tstat1)  * abs(stat2-tstat2) 
-	}
-
-    #-------- Now, generate separate posteriors for H0 and H1, and estimate params
-    sig0 = sig[which(model==nullh)]
-   	param0 = param[which(model==nullh)]
-   	stat10 = stat1[which(model==nullh)]
-   	stat20 = stat2[which(model==nullh)]
-   	model0 = model[which(model==nullh)]
-   	diff0 = diff[which(model==nullh)]
-    H0_post = order(diff0)[1:posteriorSize]
-
-    Usig		= sig[H0_post]
-	Uparam		= param[H0_post]
-	H0_post		= matrix(ncol=2, nrow=length(Usig))
-	H0_post[,1]	= Usig
-	H0_post[,2]	= Uparam
-
-	k0 	= kde(H0_post, xmin=c(0, min_param), xmax=c(max_sigma,max_param))
-
-	# Use kernel smoothing to estimate likelihood maxima.
-	k0_max_index 	= which(k0$estimate == max(k0$estimate), arr.ind = TRUE)
-	H0_est 			= c(unlist(k0$eval.points)[k0_max_index[1]], unlist(k0$eval.points)[length(k0$estimate[,1]) + k0_max_index[2]])
-
-	#------------------------------
-
-    sig1 = sig[which(model=='comp')]
-   	param1 = param[which(model=='comp')]
-   	stat11 = stat1[which(model=='comp')]
-   	stat21 = stat2[which(model=='comp')]
-   	model1 = model[which(model=='comp')]
-   	diff1 = diff[which(model=='comp')]
-    H1_post = order(diff1)[1:posteriorSize]
-
-    Usig1		= sig[H1_post]
-	Uparam1		= param[H1_post]
-	H1_post		= matrix(ncol=2, nrow=length(Usig1))
-	H1_post[,1]	= Usig1
-	H1_post[,2]	= Uparam1
-
-	k1 	= kde(H1_post, xmin=c(0, 0), xmax=c(max_sigma,max_a))
-
-	# Use kernel smoothing to estimate likelihood maxima.
-	k1_max_index 	= which(k1$estimate == max(k1$estimate), arr.ind = TRUE)
-	H1_est 			= c(unlist(k1$eval.points)[k1_max_index[1]], unlist(k1$eval.points)[length(k1$estimate[,1]) + k1_max_index[2]])
-
-	#----- Generate new datasets using fitted models
-
-	if(use_K)
-	{
-		d1 = matrix(nrow=reps, ncol=4)
-	} else {
-		d1 = matrix(nrow=reps, ncol=3)
-	}
-
-	for(i in 1:reps)
-	{
-		d = sim(tree=tree, a=H1_est[2], sigma=H1_est[1])$tval
-		s = summary_stats(tree=tree, data=d, use_K=use_K)
-		d1[i,1:2] = s[1:2]
-		d1[i,3] = 'comp'
-		if(use_K)
-		{
-			d1[i,4] = s[3]
-		}
-	}
-
-	if(use_K)
-	{
-		d0 = matrix(nrow=reps, ncol=4)
-	} else {
-		d0 = matrix(nrow=reps, ncol=3)
-	}
-	for(i in 1:reps)
-	{
-		if(nullh=='EB')
-		{
-			transformed_tree = transf.branch.lengths(tree, model='EB', parameters=list(rate=H0_est[2]))$tree
-			d = rTraitCont(transformed_tree, model='BM', sigma=H0_est[1])
-		} else if(nullh=='OU')
-		{
-			d = rTraitCont(tree, model='OU', sigma=H0_est[1], alpha=H0_est[2])
-		}
-		s = summary_stats(tree=tree, data=d, use_K=use_K)
-		d0[i,1:2] = s[1:2]
-		d0[i,3] = nullh
-		if(use_K)
-		{
-			d0[i,4] = s[3]
-		}
-	}
-
-   	newstat1 = as.numeric( c( d0[,1], d1[,1] ) )
-   	newstat2 = as.numeric( c( d0[,2], d1[,2] ) )
-   	newmodel = c( d0[,3], d1[,3] )
-
-	if(use_K)
-	{
-		newstat3 = as.numeric( c( d0[,4], d1[,4] ) )
-		newdiff = abs(newstat1-tstat1)  * abs(newstat2-tstat2) * abs(newstat3 - tstat3)
-	} else {
-		newdiff = abs(newstat1-tstat1)  * abs(newstat2-tstat2) 
-	}
-
-    # Get simulations from nth smallest distance to smallest
-    newposterior = order(newdiff)[1:post2]
-
-    # number of null sims in posterior
-    H0_lik = length(which(newmodel[newposterior]==nullh))
-
-    # number of alternate sims in posterior
-    H1_lik = length(which(newmodel[newposterior]=='comp'))
-
-    LRTstat = -2 * log(H0_lik / H1_lik)
-
-	return( data.frame(H0_est, H1_est, LRTstat) )
-}
-
-
-
-
-
-
-
-
-
-
-
 #--------------------------------------------------------------------------------------#
-#---------- Likelihood ratio: BM versus competition -----------------------------------#
-#--------------------------------------------------------------------------------------#
-oldlrt	= function(tree, data, min=0, max_sigma=10, max_a=5, reps=1e3, dt=0.001, 
-	file="sample.out", posteriorSize=500, use_K=FALSE, symp=NA)
-{
-	if(file.exists(file))
-	{
-		print(paste(file, 'already exists!'))
-		return(c())
-	}
-
-	# Simulate and write to file as we go. Single threaded.
-	for(i in 1:reps)
-   	{
- 		sig 	= runif(1, min, max_sigma)
-   		atry 	= runif(1, min, max_a)
-  		dist 	= get_dif(tree, data, atry, sig, dt=dt, use_K=use_K, symp=symp)
-	   	write(c(sig, atry, dist), file=file, append=TRUE, sep=",")
-   	}
-
-   	# Read simulations back into R
-   	x 	= read.csv(file)
-   	sig = x[,1]
-   	atry= x[,2]
-   	dist= x[,3]
-
-    # Get simulations from nth smallest to smallest
-    H1_post		= order(dist)[1:posteriorSize]
-
-    Usig		= sig[H1_post]
-	Uatry		= atry[H1_post]
-	H1_post		= matrix(ncol=2, nrow=length(Usig))
-	H1_post[,1]	= Usig
-	H1_post[,2]	= Uatry
-
-	error_bar_sig = sd(Usig)
-	error_bar_a	 = sd(Uatry)
-	error_bar = c(error_bar_sig, error_bar_a)
-
-	k 	= kde(H1_post, xmin=c(0, 0), xmax=c(max_sigma,max_a))
-	k0	= kde(H1_post, xmin=c(0, 0), xmax=c(max_sigma,0))		# sigma to max, a to 0.
-
-	# Use kernel smoothing to estimate likelihood maxima with and without competition.
-	k_max_index 	= which(k$estimate == max(k$estimate), arr.ind = TRUE)
-	H1_lik 			= k$estimate[k_max_index[1], k_max_index[2]]
-	H1_est 			= c(unlist(k$eval.points)[k_max_index[1]], unlist(k$eval.points)[length(k$estimate[,1]) + k_max_index[2]])
-
-	k0_max_index	= which(k0$estimate == max(k0$estimate), arr.ind = TRUE)
-	H0_lik 			= k0$estimate[k0_max_index[1], k0_max_index[2]]
-	H0_est 			= c(unlist(k0$eval.points)[k0_max_index[1]], unlist(k0$eval.points)[length(k0$estimate[,1]) + k0_max_index[2]])
-
-	LRT				= -2 * log( H0_lik / H1_lik )
-
-	file.remove(file)
-
-	return( data.frame(H0_est, H0_lik, H1_est, H1_lik, LRT) )
-}
-
-#--------------------------------------------------------------------------------------#
-#---------------------- Legacy functions. Don't use these. ----------------------------#
-#--------------------------------------------------------------------------------------#
-
-genTree	= function(tree, a=0, sigma=1, sigma2 = 1, dt=1, nTraits=1, kernel="CE", lim=0) 
-{
-	x = data.frame( sim(tree=tree, dt=0.01*dt, sigma=sigma, a=a, ntraits=nTraits)$tval )
-	names(x) = 'traits'
-	return(x)
-}
-
-asVCV=as_vcv
-rUMT=rand_umt
-
-LRT = function(tree, data, a, sigma, dt=0.01, nTraits, kernel, lim, sstat, 
-			   reps, posteriorSize, max_sigma=5, max_a=5, min=0, file='sample.out')
-{
-	return(lrt(tree=tree, data=data, min=min, max_sigma=max_sigma, max_a=max_a, 
-			   reps=reps, dt=dt, file=file, posteriorSize=posteriorSize, use_K=FALSE))
-}
-
-#--------------------------------------------------------------------------------------#
-
-
 
